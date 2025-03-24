11810da6dd24cf97996d5fbde6c7bedf4e45f388
magog-1 <maksim.25705@gmail.com>
2025-03-24 21:04:43 +0300

    env modded

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,17 @@
 import os
+from environs import Env
 from pydantic_settings import BaseSettings
+
+env = Env()
+env.read_env()
 
 class Settings(BaseSettings):
     JWT_SECRET: str = os.getenv("JWT_SECRET", "myjwtsecret")
-    JWT_ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60*24  # время жизни токена в минутах
+    JWT_ALGORITHM: str = env.str("JWT_ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = env.int("ACCESS_TOKEN_EXPIRE_MINUTES")  # время жизни токена в минутах
 
-    SQLALCHEMY_DATABASE_URL: str = "sqlite:///./test.db"
-    CELERY_BROKER_URL: str = "redis://localhost:6379/0"
-    CELERY_RESULT_BACKEND: str = "redis://localhost:6379/0"
+    SQLALCHEMY_DATABASE_URL: str = env.str("SQLALCHEMY_DATABASE_URL")
+    CELERY_BROKER_URL: str = env.str("CELERY_BROKER_URL")
+    CELERY_RESULT_BACKEND: str = env.str("CELERY_RESULT_BACKEND")
 
 settings = Settings()
